619a345643bd083f4e9488c35691df72e150e62f
gray <graylearnone@gmail.com>
2024-10-16 01:16:08 -0400

    Initial redux and slice

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Homescreen</Text>
+      <Text>Pickme!</Text>
+    </View>
+  );
+};
+// const styles = StyleSheet.create({});
+export default HomeScreen;
