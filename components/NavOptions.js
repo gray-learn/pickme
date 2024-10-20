93ab5ba15a5f6773ac3dbc839e9680df7ce279b5
gray <graylearnone@gmail.com>
2024-10-20 01:57:27 -0400

    feat/navigation_stack

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,6 +3,7 @@ import React from "react";
 import { FlatList } from "react-native-web";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -19,13 +20,19 @@ const data = [
   },
 ];
 const NavOptions = () => {
+  const navigation = useNavigation();
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.id} // rednder the upon key
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate(item.screen);
+          }}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
@@ -35,7 +42,7 @@ const NavOptions = () => {
           </View>
           <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
           <Icon
-            style={tw`p-2 bg-black  rounded-full w-10 mt-4`}
+            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
             name="arrowright"
             color="white"
             type="antdesign"
